9b0ad3be92780aca8ca090ec5637577ffe355c2d
Ben Shuyi Chen <benshuyichen@gmail.com>
2020-09-12 00:05:28 -0700

    Add radar chart

--- src/components/chart/Chart.stories.tsx
+++ src/components/chart/Chart.stories.tsx
@@ -2,14 +2,14 @@ import React from "react";
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from "@storybook/react/types-6-0";
 
-import App from "./App";
+import Chart from "./Chart";
 
 export default {
-  title: "Components/App",
-  component: App,
+  title: "Components/Chart",
+  component: Chart,
 } as Meta;
 
-const Template: Story = (args) => <App {...args} />;
+const Template: Story = (args) => <Chart {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {};
